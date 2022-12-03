53559a87a37c0e0ff9cbe6c9a88cc23cd58dbf70
Harsh Kumar <hadron43@yahoo.com>
2022-12-03 18:02:47 +0530

    change to json response

--- manager/scaling_manager/views.py
+++ manager/scaling_manager/views.py
@@ -1,17 +1,22 @@
 import threading
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.core.cache import cache
 from .server_classes import ServerManager
 
 def available_gameserver(request):
     gameserver_manager = cache.get("gameserver_manager")
-    return HttpResponse(gameserver_manager.get_available_server().address)
+    return JsonResponse({
+        'available': gameserver_manager.get_available_server().address
+    })
 
 def available_gameserver_list(request):
     gameserver_manager = cache.get("gameserver_manager")
     server_list = gameserver_manager.get_available_servers()
-    return HttpResponse('\n'.join(s.address for s in server_list))
+    ret_list = [s.address for s in server_list]
+    return JsonResponse({
+        'server_list': ret_list
+    })
 
 def server_update(request):
     if(cache.get("thread_running")):
